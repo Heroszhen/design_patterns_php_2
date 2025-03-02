df457f8824030a07cde03dfe424c436185992ab1
zhen <zhen.yang@hotmail.fr>
2025-03-02 13:11:50 +0100

    add srp

--- SOLID/SRP.php
+++ SOLID/SRP.php
@@ -1,2 +1,18 @@
 <?php
+//Responsabilité unique(Single-responsibility principle)
+//Chaque classe ou fonction a ses propres fonctionnalités, par exemple une classe Article doit contenir les attributs et les méthodes concernant les articles, on ne mets pas ceux et celles de la classe User(genre "email", "password")
+//Le code est plus propre et robustre, et les devs peuvent comprendre rapidement et facilement
 
+class User 
+{
+    private int $id;
+    private string $name;
+    private int $age;
+}
+class Article 
+{
+    private int $id;
+    private string $title;
+    private string $content;
+    private User $author;
+}
