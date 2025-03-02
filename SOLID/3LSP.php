b4de7d9b570d153eb18f1dcff8b3648a161b2359
zhen <zhen.yang@hotmail.fr>
2025-03-02 16:32:34 +0100

    add lsp

---
+++ SOLID/3LSP.php
@@ -0,0 +1,5 @@
+<?php
+//Substitution de Liskov (Liskov substitution principle)
+//les objets d'une classe enfant doivent pouvoir remplacer les objets d'une classe parent sans nuire à l'intégrité de l'application, l'enfant ne doit pas changer le comportement du parent
+//pour la maintenance du code
+//pour réutiliser le code
